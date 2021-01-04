8f21ed155ebcf3c0947db13318291a20c66488e6
Takanori Nakanowatari <nakanowatari@gmail.com>
2021-01-04 21:31:46 +0900

    first step

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+/Users/takanori/Workspace/Rust/hello/target/debug/deps/hello: src/main.rs
+
+/Users/takanori/Workspace/Rust/hello/target/debug/deps/hello.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/takanori/Workspace/Rust/hello/target/debug/hello: /Users/takanori/Workspace/Rust/hello/src/main.rs
